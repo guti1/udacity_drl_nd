dccdb1e6d17fefd6e7a362903c72b1a3d802dbf8
guti1 <guti1@freemail.hu>
2020-08-18 11:12:38 +0200

    major restructure to be able to define hidden layers structure by lists

--- exercises/projects/project2_continous_control/solution/model.py
+++ exercises/projects/project2_continous_control/solution/model.py
@@ -4,79 +4,98 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def hidden_init(layer):
-    fan_in = layer.weight.data.size()[0]
-    lim = 1. / np.sqrt(fan_in)
-    return (-lim, lim)
 
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=512, fc2_units=256, fc3_units=128):
+    def __init__(self, state_size, action_size, seed, hidden_layers, use_batch_norm):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
-            fc3_units (int): Number of nodes in third hidden layer
+            hidden layers([int]): list of the sizes each hidden layers
+            batch_norm (bool): use or not batch norm between layers
         """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, action_size)
-        self.reset_parameters()
+        self.batch_norm = use_batch_norm
 
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        def _init_weights(m):
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                m.bias.data.fill_(0.01)
+
+        # Construction of the NN conditionally on batch-norm
+        if self.batch_norm:
+            layers = [state_size] + hidden_layers
+            # Input layer
+            self.hidden_layers = nn.ModuleList([nn.BatchNorm1d(state_size)])
+            # Remaining hidden layers
+            for i in range(len(layers) - 1):
+                self.hidden_layers.extend([nn.Linear(layers[i], layers[i + 1])])
+                self.hidden_layers.extend([nn.BatchNorm1d(layers[i + 1])])
+
+        # Construction without batch-norm
+        else:
+            self.hidden_layers = nn.ModuleList([nn.Linear(state_size, hidden_layers[0])])
+            layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+            self.hidden_layers.extend([nn.Linear(n1, n2) for n1, n2 in layer_sizes])
+
+        # Finally the output layer
+        self.output = nn.Linear(hidden_layers[-1], action_size)
+
+        # Weight init
+        self.hidden_layers.apply(_init_weights)
+        self.output.apply(_init_weights)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        return torch.tanh(self.fc4(x))
+        x = state
 
+        if self.batch_norm:
+            x = self.hidden_layers[0](x)
+            for i in range(1, len(self.hidden_layers)-1, 2):
+                x = F.relu(self.hidden_layers[i + 1](self.hidden_layers[i](x)))
+        else:
+            for j in self.hidden_layers:
+                x = F.relu(j(x))
+
+        return torch.tanh(self.output(x))
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=512, fc2_units=256, fc3_units=128):
+    def __init__(self, state_size, action_size, seed, hidden_layers=[400,300]):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer
-            fc3_units (int): Number of nodes in the third hidden layer
+            hidden layers([int]): list of the sizes each hidden layers
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, 1)
-        self.reset_parameters()
 
-    def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
+
+        self.hidden_layers = nn.ModuleList(
+            [nn.Linear(state_size, hidden_layers[0]), nn.Linear(hidden_layers[0] + action_size, hidden_layers[1])])
+        layer_sizes = zip(hidden_layers[1:-1], hidden_layers[2:])
+        self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+
+        # Finally the output layer
+        self.output = nn.Linear(hidden_layers[-1], 1)
+
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))
-        x = torch.cat((xs, action), dim=1)
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        return self.fc4(x)
+
+        x = F.relu(self.hidden_layers[0](state))
+        x = torch.cat((x, action), dim=1)
+
+        for i in range(1, len(self.hidden_layers)):
+            x = F.relu(self.hidden_layers[i](x))
+
+        return self.output(x)
